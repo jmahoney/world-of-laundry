a30d97962d164b06299310dd7a71b0d8dd751a3e
Joe Mahoney <jmahoney@iterative.co.nz>
2014-08-01 22:45:13 +1200

    generation basically there

--- generate.rb
+++ generate.rb
@@ -1,20 +1,48 @@
 #!/usr/bin/env ruby
 require "json"
-# go through the corpus, find all the cloth items, put them in a text file
+
+# read laundry care into instructions and warningd
+instructions = []
+warnings = []
+
+json = File.read("laundry_care.json")
+JSON.parse(json)["laundry_care_instructions"].map do |i|
+    txt = i['instruction']
+    txt.start_with?("Do Not") ? warnings << txt : instructions << txt
+end
+
+generator = Random.new
+
+good_flare = []
+File.open("good_flare.txt", "r").each_line{|l| good_flare << l}
+bad_flare = []
+File.open("bad_flare.txt", "r").each_line{|l| bad_flare << l}
 
 ARMOUR = 4
 CLOTH = 1
-
-instructions = JSON.parse(File.read("laundry_care.json"))["laundry_care_instructions"]
+WOWHEAD = "http://www.wowhead.com/item="
 
 tweet_file = File.open("index.txt", "w")
-
 Dir.glob("corpus/*.json").entries.each do |f|
     begin
+        if generator.rand(0..5) > 0
+            care_text = instructions[generator.rand(0..instructions.length-1)]
+            care_text += ": "
+            care_text += good_flare[generator.rand(0..good_flare.length-1)]
+        else
+            care_text = warnings[generator.rand(0..warnings.length-1)]
+            care_text += ": "
+            care_text += bad_flare[generator.rand(0..bad_flare.length-1)]
+        end
+
         item = JSON.parse(File.read(f))
         if (item['itemClass'] == ARMOUR && item['itemSubClass'] == CLOTH)
-            name = "#{item['name']}"
-            tweet_file.write(name + "\n")
+
+            name = "#{item['name']}\n"
+            name += "#{care_text}"
+            name += "#{WOWHEAD}#{File.basename(f, '.json')}\n\n"
+            puts name
+            tweet_file.write(name)
         end
     rescue
     end
